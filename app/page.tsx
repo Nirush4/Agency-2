e1fea53505b79eef7bdd807eaba07abcc0e86dcf
03-renate <renate.pedersen03@gmail.com>
2026-03-16 15:34:46 +0100

    feat: design-system setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,18 @@ export default function Home() {
   return (
     <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
       <h1 className='text-6xl'>Hi World</h1>
+
+      <h1 className="text-h1 font-heading">
+        Title
+      </h1>
+
+      <p className="text-body font-body">
+        Paragraph text
+      </p>
+
+      <span className="text-small text-gray-500">
+        Detail text
+      </span>
     </div>
   );
 }
