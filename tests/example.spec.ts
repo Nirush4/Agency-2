5556647b2899d1b961f2ef60eb6f3c018d3a2e67
SanderTorg <santorgersen@gmail.com>
2026-03-17 15:30:48 +0100

    testing playwright

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { LOCAL_BASE_URL } from "@/playwright.config";
 import { test, expect } from "@playwright/test";
 
-test("has project title", async ({ page }) => {
+test("has project title - Create Next App", async ({ page }) => {
   await page.goto(LOCAL_BASE_URL);
 
   await expect(page).toHaveTitle(/Create Next App/);
