807e7398adab15ce5625fecdd3ed53e4f62ad259
SanderTorg <santorgersen@gmail.com>
2026-03-17 15:14:09 +0100

    updated the naming for middleware to proxy since nextjs complained.

--- proxy.ts
+++ proxy.ts
@@ -4,7 +4,7 @@ import { type NextRequest, NextResponse } from "next/server";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export async function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request: {
       headers: request.headers,
