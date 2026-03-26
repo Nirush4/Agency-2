855fd65082f05300ca54c8d1c7f86933fd330e09
SanderTorg <santorgersen@gmail.com>
2026-03-26 14:35:05 +0100

    updated create

--- lib/db/create.ts
+++ lib/db/create.ts
@@ -10,10 +10,10 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Function to create a new post
-export async function createPost(title: string, content: string) {
+export async function createPost(post: Post) {
   const { data, error } = await supabase
     .from("recipes")
-    .insert([{ title: title, content: content }])
+    .insert([{ title: post.title, content: post.content }])
     .select();
 
   if (error) {
